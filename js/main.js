7098e324ac09a9b28efb086f35f23cdb8a6489e9
Israel Santizo <joseisraelss.jiss@gmail.com>
2021-01-03 19:10:20 -0600

    Se crea logica para colocar un gift por unos segundos

---
+++ js/main.js
@@ -0,0 +1,79 @@
+//Obtner la respuesta del usuario
+document.querySelector('.js-go').addEventListener('click', function(){
+    var input = document.querySelector('input').value;
+    var userInput = getUserInput();
+    alert(userInput);
+    searchGiphy(userInput);
+
+});
+function busqueda(){
+    switch(userInput){
+        case '1':
+            console.log('Escogio 1');
+            alert('Escogio 1');
+            break;
+        case '2':
+            console.log('Escogio 2');
+            break;
+    }
+}
+//Reconcer el click del teclado
+document.querySelector('.js-userinput').addEventListener('keyup', function(e){
+    //The e in the function es the data for de event console.log(e);
+    //var input = document.querySelector('input').value;
+    //The number 13 is the code for enter in the keyboard
+    //The keycode in js not compatible for all browsers
+    if(e.which === 13){
+        var userInput = getUserInput();
+        searchGiphy(userInput);
+    }else{
+        //alert('N era el enter');
+    }
+});
+function getUserInput() {
+    var inputValue = document.querySelector('.js-userinput').value;
+    switch(inputValue){
+        case '1':
+            console.log('Escogio 1');
+            alert('Escogio 1');
+            break;
+        case '2':
+            console.log('Escogio 2');
+            break;
+    }
+	return inputValue;
+}
+//Credenciales para ingresar al API
+function searchGiphy(searchQuery) {
+    var url = "https://api.giphy.com/v1/gifs/search?q="+ searchQuery +"&api_key=Rhp6uPOa1OBOgfjv7Gl7neJ8gTH2BuSs&rating=g&lang=en";
+    var GiphyAJAXCall = new XMLHttpRequest();
+	GiphyAJAXCall.open( 'GET', url );
+	GiphyAJAXCall.send();
+	GiphyAJAXCall.addEventListener('load', function( data ) {
+			var actualData = data.target.response;
+			pushToDOM(actualData);
+			console.log(actualData);
+	});
+}
+//searchGiphy( "cats" );
+function pushToDOM(input){
+    var response = JSON.parse(input);
+    //alert(input);
+    var images = response.data;
+    //var imageUrl = response.data[1].images.fixed_height.url;
+    //var imageUrls = response.data;
+    var container = document.querySelector('.giftScreen');
+    //console.log(imageUrl);
+    //var src = images.fixed_height.url
+    for(i = 0; i<5; i++) {
+    container.innerHTML= " ";
+        // concatenate a new IMG tag
+        setTimeout(function(){
+                var src = images[i].images.fixed_height.url;
+                container.innerHTML = " ";
+                container.innerHTML += "<img src='"+ src +"' class='container__image' />";
+                i++;
+        }, 3000*i);
+    }
+    //container.innerHTML += "<img src=\"" + imageUrl + "\" class=\"container-image\">";
+}
